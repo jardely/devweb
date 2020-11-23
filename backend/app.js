86a49dd1fc37b7d0ec398a78f82a67bd548aca26
Jardely Maris <jardely.santos@splab.ufcg.edu.br>
2020-11-23 17:11:44 -0300

    Adicionar cors

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
+var cors = require('cors')
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(cors())
 
 app.use('/commerce', indexRouter);
 
-PORT = 8080;
+PORT = 8081;
 app.listen(PORT, function () {
   console.log('Servidor em execução na porta ' + PORT);
 });
